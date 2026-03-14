cf02e511270886fcc269c3417451c0f7a18f9232
Ladipo Ezekiel Ipadeola <rileydrizzy@hotmail.com>
2026-03-14 09:12:17 +0000

    fix: update comments in set_env_variables.sh for clarity on API access

--- set_env_variables.sh
+++ set_env_variables.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# Set environment variables for Kaggle API access
+# Set environment variables for Kaggle, WanB API access
 # This script can be sourced to set the environment variable in the current shell session
 # Usage: source set_env_variables.sh
-# NOTE: Ensure that you make a copy of this script and replace the placeholder with your actual Kaggle API token before sourcing it.
+# NOTE: Ensure that you make a copy of this script and replace the placeholder with your actual APIs token before sourcing it.
 
 # Replace 'xxxxxxxxxxxxxx' with your actual Kaggle API token
 export KAGGLE_API_TOKEN=xxxxxxxxxxxxxx
+
